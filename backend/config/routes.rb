c2e7e596e68ee3ffe9b3e9d2daa54f73080a8c7a
Edimo Sousa Silva <edimossilva@gmail.com>
2021-01-05 22:18:25 +0200

    Recreate backend project with rails 6.1 and ruby 3  (#19)
    * :tada: recreate rails project
    
    * 🚨 lint
    
    * ✅ configure rspec

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-  post '/auth/login', to: 'authentication#login'
+# frozen_string_literal: true
 
-  mount ActionCable.server => '/cable'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
