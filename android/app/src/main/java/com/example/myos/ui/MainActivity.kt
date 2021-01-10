465d4ef3f4c31b2ba329b670ba61ac42a11d3b27
Roandersonxd1 <roandersonfelipe@gmail.com>
2021-01-10 18:01:46 -0300

    Layout login (#35)
    * Teste KTLINT android
    
    * Test Grandlew
    
    * :art: Create layout login

---
+++ android/app/src/main/java/com/example/myos/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myos.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
