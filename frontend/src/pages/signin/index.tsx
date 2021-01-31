194f894fdb268e31965c3a71310f9a452d07e5c7
Leonardo Brito <f.leobrito@gmail.com>
2021-01-31 16:25:37 -0300

    Introducing apollo service configuration (#69)
    * :sparkles: Introducing apollo service configuration
    
    * :fire: Removing code

---
+++ frontend/src/pages/signin/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`
+const SignIn: React.FC = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :( </p>
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ))
+}
+
+export default SignIn
